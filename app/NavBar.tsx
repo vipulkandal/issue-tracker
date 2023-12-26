99d4bbd6a98350b80c74506fb8a17a86a60cd9eb
vipulkandal <vipulkandal@gmail.com>
2023-12-26 23:50:20 +0530

    Styling in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: "Dashboard",
@@ -22,7 +28,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900": link.href === currentPath, // render if true
+                "text-zinc-500": link.href !== currentPath, // render if false
+                "hover:text-zinc-800 transition-colors": true, // true means this class will always render
+              })}
               href={link.href}
             >
               {link.label}
